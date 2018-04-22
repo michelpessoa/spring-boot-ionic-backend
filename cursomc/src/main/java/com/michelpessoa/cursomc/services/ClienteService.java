706d9b73651050385f5714c946355363c51acb84
Michel Pessoa <michelpessoa@gmailcom>
2018-04-22 19:46:15 -0300

    Endpoint /clientes/{id} disponivel

---
+++ cursomc/src/main/java/com/michelpessoa/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.michelpessoa.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.michelpessoa.cursomc.domain.Cliente;
+import com.michelpessoa.cursomc.repositories.ClienteRepository;
+import com.michelpessoa.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
