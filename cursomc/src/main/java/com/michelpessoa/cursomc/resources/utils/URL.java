8049c1aa658c51fddfdddc1773710937dca78314
Michel Pessoa <michelpessoa@gmailcom>
2018-04-28 21:35:45 -0300

    Busca de produtos por nome e categorias

---
+++ cursomc/src/main/java/com/michelpessoa/cursomc/resources/utils/URL.java
@@ -0,0 +1,28 @@
+package com.michelpessoa.cursomc.resources.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class URL {
+
+	public static String decodeParam(String s) {
+		try {
+			return URLDecoder.decode(s, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			return "";
+		}
+	}
+
+	public static List<Integer> decodeIntList(String s) {
+		/*
+		 * String[] vet = s.split(","); List<Integer> list = new ArrayList<>();
+		 * for(String x : vet) { list.add(Integer.parseInt(x)); } return list;
+		 * Equivalênvia de código logo abaixo.
+		 */
+		return Arrays.asList(s.split(",")).stream().map(x -> Integer.parseInt(x)).collect(Collectors.toList());
+	}
+
+}
